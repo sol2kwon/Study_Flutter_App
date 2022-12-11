7488cdf4ac6dddab38bf7e0444e815a08dbaba34
sol2kwon <amorfatisole@gmail.com>
2022-12-11 18:18:23 +0900

    20221211

--- shazam/lib/main.dart
+++ shazam/lib/main.dart
@@ -183,12 +183,44 @@ class SecondTab extends StatelessWidget {
                 ],
               ),
             ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.1),
             Text(
               "Shazam하려면 탭하세요",
               style: TextStyle(
                   color: Colors.white,
                   fontSize: 24,
                   fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.06),
+            Container(
+              alignment: Alignment.center,
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.blue[300],
+                shape: BoxShape.circle,
+              ),
+              child: Image.network(
+                "https://i.ibb.co/hxNbZ8p/shazam.png",
+                color: Colors.white,
+                width: 130,
+                height: 130,
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.12),
+            Container(
+              width: 30,
+              height: 50,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue[400],
+              ),
+              child: Icon(
+                Icons.search,
+                size: 20,
+                color: Colors.white,
+              ),
             )
           ],
         ),
